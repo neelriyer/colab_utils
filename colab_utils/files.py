0a7f615845811ec516d71c02137d5364420ce713
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-03-26 07:15:49 +1100

    init commit

---
+++ colab_utils/files.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import shutil
+
+
+def copy_file_to_drive(file, output_folder, overwrite = False):
+  drive_path = Path('/content/drive/MyDrive/')
+  if not drive_path.exists(): raise 'drive not found. please mount drive.'
+  base_path = drive_path/output_folder
+  base_path.mkdir(parents = True, exist_ok = True)
+  if (base_path/Path(file).name).exists() and overwrite: os.remove(base_path/Path(file).name)
+  shutil.copyfile(file, str(base_path/Path(file).name))
+
+
+
