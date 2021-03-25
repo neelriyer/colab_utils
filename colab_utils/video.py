7f6d4de0e044fd1878f97f2d440360008f4f58fd
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-03-26 07:49:59 +1100

    clean up

--- colab_utils/video.py
+++ colab_utils/video.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from IPython.display import HTML
 from base64 import b64encode
-from ._video_to_frames import print_progress, extract_frames
+from ._video_to_frames import video_to_frames
 import os
 import cv2
 
@@ -29,52 +29,8 @@ def show_short_video(file, seconds = 10):
   """ % data_url)
 
 
-# from https://gist.github.com/HaydenFaulkner/54318fd3e9b9bdb66c5440c44e4e08b8#file-video_to_frames-py
-def video_to_frames(video_path, frames_dir, overwrite=False, every=1, chunk_size=1000):
-
-    """
-    Extracts the frames from a video using multiprocessing
-
-    :param video_path: path to the video
-    :param frames_dir: directory to save the frames
-    :param overwrite: overwrite frames if they exist?
-    :param every: extract every this many frames
-    :param chunk_size: how many frames to split into chunks (one chunk per cpu core process)
-    :return: path to the directory where the frames were saved, or None if fails
-    """
-
-    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
-    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
-
-    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
-
-    # make directory to save frames, its a sub dir in the frames_dir with the video name
-    # os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
-
-    capture = cv2.VideoCapture(video_path)  # load the video
-    total = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))  # get its total frame count
-    capture.release()  # release the capture straight away
-
-    if total < 1:  # if video has no frames, might be and opencv error
-        print("Video has no frames. Check your OpenCV + ffmpeg installation, can't read videos!!!\n"
-              "You may need to install OpenCV by source not pip")
-        return None  # return None
-
-    frame_chunks = [[i, i+chunk_size] for i in range(0, total, chunk_size)]  # split the frames into chunk lists
-    frame_chunks[-1][-1] = min(frame_chunks[-1][-1], total-1)  # make sure last chunk has correct end frame
-
-    prefix_str = "Extracting frames from {}".format(video_filename)  # a prefix string to be printed in progress bar
-
-    # execute across multiple cpu cores to speed up processing, get the count automatically
-    with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
-
-        futures = [executor.submit(extract_frames, video_path, frames_dir, overwrite, f[0], f[1], every)
-                   for f in frame_chunks]  # submit the processes: extract_frames(...)
-
-        for i, f in enumerate(as_completed(futures)):  # as each process completes
-            print_progress(i, len(frame_chunks)-1, prefix=prefix_str, suffix='Complete')  # print it's progress
-
-    return os.path.join(frames_dir, video_filename)  # when done return the directory containing the frames
+def frame_extractor(video_path, frames_dir, overwrite=False, every=1, chunk_size=1000):
+  video_to_frames(video_path, frames_dir, overwrite, every, chunk_size)
 
 
 
