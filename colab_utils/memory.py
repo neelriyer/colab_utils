48cdbd0bf0f8b1a7df7567f3624f43b5f2de96e9
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2021-04-02 15:27:53 +1100

    add combine_videos utils

--- colab_utils/memory.py
+++ colab_utils/memory.py
@@ -1,6 +1,8 @@
 import gc
 import torch
 
+__all__ = ['free_mem']
+
 def free_mem():
 	gc.collect()
 	torch.cuda.empty_cache()
--- colab_utils/video.py
+++ colab_utils/video.py
@@ -9,35 +9,35 @@ __all__ = ['show_short_video', 'video_to_frames', 'combine_videos']
 
 def show_short_video(file, seconds = 10):
 
-  path = Path(file)
+	path = Path(file)
 
-  new_file_name = path.stem + '_short' + path.suffix
-  output_file = path.parents[0]/str(new_file_name)
-  print(output_file)
+	new_file_name = path.stem + '_short' + path.suffix
+	output_file = path.parents[0]/str(new_file_name)
+	print(output_file)
 
-  end_time = '00:00:'+str(seconds)
-  cmd = 'ffmpeg -loglevel warning -y -ss 00:00:00 -i '+str(file)+' -c copy -t '+str(end_time) + ' ' + str(output_file)
-  os.system(cmd) # TODO use ffmpy instead of os.system
-  
-  mp4 = open(output_file,'rb').read()
-  data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-  return HTML("""
-  <video width=400 controls>
-        <source src="%s" type="video/mp4">
-  </video>
-  """ % data_url)
+	end_time = '00:00:'+str(seconds)
+	cmd = 'ffmpeg -loglevel warning -y -ss 00:00:00 -i '+str(file)+' -c copy -t '+str(end_time) + ' ' + str(output_file)
+	os.system(cmd) # TODO use ffmpy instead of os.system
+	
+	mp4 = open(output_file,'rb').read()
+	data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+	return HTML("""
+	<video width=400 controls>
+				<source src="%s" type="video/mp4">
+	</video>
+	""" % data_url)
 
 def frame_extractor(video_path, frames_dir, overwrite=False, every=1, chunk_size=1000):
-  video_to_frames(video_path, frames_dir, overwrite, every, chunk_size)
+	video_to_frames(video_path, frames_dir, overwrite, every, chunk_size)
 
 def combine_videos(folder, output, merge_file = 'merge_file.txt'):
 
-  # write all videos to txt
-  _write_videos_to_txt(merge_file, folder)
+	# write all videos to txt
+	_write_videos_to_txt(merge_file, folder)
 
-  # combine videos (TODO: replace with ffmpy)
-  cmd = 'ffmpeg -loglevel warning -y -f concat -safe 0 -i ' + str(merge_file) + ' -c copy ' + str(output)
-  subprocess.run(cmd.split(' '))
+	# combine videos (TODO: replace with ffmpy)
+	cmd = 'ffmpeg -loglevel warning -y -f concat -safe 0 -i ' + str(merge_file) + ' -c copy ' + str(output)
+	os.system(cmd)
 
 
 
